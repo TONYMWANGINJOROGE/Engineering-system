f880f18e81d9253d523575e1029b6ce92da0b455
TONYMWANGINJOROGE <157480206+TONYMWANGINJOROGE@users.noreply.github.com>
2024-03-14 13:10:17 +0300

    Add files via upload

---
+++ django7/Users/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+def aboutus(request):
+    return render(request, 'aboutus.html')
+def contacts(request):
+    return render(request, 'contacts.html')
+def register(request):
+    return render(request, 'register.html')
+def logout(request):
+    return render(request, 'logout.html')
+
