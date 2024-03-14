d108fcbc6b6837f8c063497c03b4782551f58b86
TONYMWANGINJOROGE <157480206+TONYMWANGINJOROGE@users.noreply.github.com>
2024-03-14 12:51:13 +0300

    Add files via upload

---
+++ datascience/intro.py
@@ -0,0 +1,6 @@
+print("Welcome to eMobilis Mobile")
+print("My name is Kimutai Kiprop")
+myschool="kapsabet boys high school"
+print(myschool)
+myname="kimutai kiprop"
+print("My name is Kimutai Kiprop")
